4311517fb7ff2ed9ff41545d468f0fbdfead0258
Yudha Indra <165381655+yudhaindraap@users.noreply.github.com>
2025-11-30 21:46:05 +0700

    pembaruan fungsi realtime lebih ok(tidak harus buka app)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-56da8dea'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5uff9uge9sg"
+    "revision": "0.k792nl2tj4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
